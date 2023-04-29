7784f89cc5817c36ea3bd847c550f62d007c4596
Roman Koshchei <romankoshchei@gmail.com>
2023-04-29 11:16:20 +0300

    operator overloading, but unfortunate it doesn't work with generics, so created linear filter

---
+++ Unator/Chain/Showcase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unator.Chain;
+
+/// <summary>
+/// Just showcase of use Linear Chain
+/// </summary>
+internal class Showcase
+{
+    public static void Main()
+    {
+        var chain = LinearChain<int, string>
+            .Start(input => (input + 1).ToString())
+            .Next(input => new { TestAnonymus = true })
+            .Next(input => input.TestAnonymus);
+
+        var result = chain.Run(-46);
+
+        var filter = new LinearFilter<int, bool>()
+            | IsNotNegative
+            | IsLess100
+            | IsEven;
+
+        var filterResult = filter.Run(-46);
+
+        if (filterResult.All(x => x))
+        {
+            Console.WriteLine("Passed all filters");
+        }
+    }
+
+    public static bool IsNotNegative(int input) => input >= 0;
+
+    public static bool IsLess100(int input) => input < 100;
+
+    public static bool IsEven(int input) => input % 2 == 0;
+}
